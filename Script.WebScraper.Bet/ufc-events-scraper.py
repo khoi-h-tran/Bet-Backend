cc219718d7900d30c5d999ea7221c7c6ef73137b
khoi-h-tran <khoi.huynh.tran@gmail.com>
2022-04-21 19:59:06 -0400

    Add Events scraping & test html pages

--- Script.WebScraper.Bet/ufc-events-scraper.py
+++ Script.WebScraper.Bet/ufc-events-scraper.py
@@ -1,3 +1,6 @@
+
+from datetime import datetime
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,17 +23,30 @@ class Card:
         self.cardType = cardType
         self.cardEvents = []
 
-class UFCEvents:
+class UFCEvent:
     def __init__(self, eventName, eventDate, eventVenue, eventCards):
         self.eventName = eventName
         self.eventDate = eventDate
         self.eventVenue = eventVenue
         self.eventCards = []
 
-url = "https://www.ufc.com/event/ufc-274"
+# ========= Pulling from website =====================
+# url = "https://www.ufc.com/event/ufc-274"
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+# result = requests.get(url)
+# eventPage = BeautifulSoup(result.text, "html.parser")
+
+# ========= Writing html to file =====================
+# url = "https://www.ufc.com/event/ufc-274"
+
+# result = requests.get(url)
+# doc = BeautifulSoup(result.text, "html.parser")
+# html = doc.prettify("utf-8")
+
+# with open("Main_Card_Page.html", "wb") as file:
+#     file.write(html)
+
+# ========= Read Locally =====================
 # print(doc.prettify())
         
 # with is short-form way to work with files with a built in error handler
@@ -40,8 +56,69 @@ doc = BeautifulSoup(result.text, "html.parser")
 
 ufcEventsList = []
 
-def buildFighters(doc):
-    fightEvents = doc.find_all("li", class_="l-listing__item")
+# ========= On Events Page - Reads all upcoming events =====================
+with open("Events_Page.html", encoding='utf8') as file:
+    eventsPage = BeautifulSoup(file, "html.parser") 
+
+eventsPage.prettify("utf-8")
+
+eventsList = eventsPage.find_all("li", class_="l-listing__item")
+
+for index, eventListItem in enumerate(eventsList):
+    if index == 3 : break
+    linkDiv = eventsList[index].find("h3", class_="c-card-event--result__headline")
+    linkHrefEl = linkDiv.find("a", href=re.compile("/event/ufc"))
+    link = f"https://www.ufc.com{linkHrefEl.get('href')}"
+
+    eventType = ""
+    matchUp = linkHrefEl.getText().strip()
+
+    if "fight-night" in link:
+        eventType = "Fight Night"
+    elif "ufc" in link:
+        stringArray = link.split("-")
+        eventNumber = stringArray[len(stringArray) - 1]
+        eventType = f"UFC {eventNumber}"
+    else:
+        print("no event found")    
+
+    dateDiv = eventsList[index].find("div", class_="c-card-event--result__date tz-change-data")
+    timeStampMain = dateDiv.get("data-main-card-timestamp")
+
+    date = "N/A"
+
+    if timeStampMain != "":
+        dateTime = datetime.fromtimestamp(int(timeStampMain))
+        date = str(dateTime).replace(" ", "T")
+        timeMain = datetime.fromtimestamp(int(timeStampMain)).strftime('%#I%p')
+        #print(f"timeMain: {timeMain}")
+
+    timeStampPrelims = dateDiv.get("data-prelims-card-timestamp")
+    if timeStampPrelims != '':
+        timePrelims = datetime.fromtimestamp(int(timeStampPrelims)).strftime('%#I%p')
+        #print(f"timePrelims: {timePrelims}")
+
+    timeStampEarlyPrelims = dateDiv.get("data-early-card-timestamp")
+    if timeStampEarlyPrelims != '':
+        timeEarlyPrelims = datetime.fromtimestamp(int(timeStampEarlyPrelims)).strftime('%#I%p')
+        #print(f"timeEarlyPrelims: {timeEarlyPrelims}")
+
+    venueDiv = eventsList[index].find("div", class_="field field--name-taxonomy-term-title field--type-ds field--label-hidden field__item")
+    venue = venueDiv.getText().strip()
+
+    eventName = f"{eventType} - {matchUp}"
+    eventDate = date 
+    eventVenue = venue
+
+    print(link)
+    print(f"eventName: {eventName}")
+    print(f"eventDate: {eventDate}")
+    print(f"eventVenue: {eventVenue}\n")
+
+# ========= On Event Page - Reads all fighters from event =====================
+
+def buildFighters(eventPage):
+    fightEvents = eventPage.find_all("li", class_="l-listing__item")
     fighterNamesList = getFighterNames(fightEvents)
     print(fighterNamesList)
     fighterImageList = getFighterImages(fightEvents)
@@ -89,4 +166,4 @@ def getFighterImages(fightEvents):
             # print(image['src'])
     return fighterImageList
 
-buildFighters(doc)
+#buildFighters(eventPage)
