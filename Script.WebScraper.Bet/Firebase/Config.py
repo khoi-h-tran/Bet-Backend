4fd07ae5d56713eb4e2a6306ab1b828a09ab8c33
khoi-h-tran <khoi.huynh.tran@gmail.com>
2022-08-22 17:05:51 -0400

    Authenticates & Writes Test Data to BetApp DB

--- Script.WebScraper.Bet/Firebase/Config.py
+++
@@ -1,6 +0,0 @@
-config = {
-  "apiKey": "AIzaSyCA3-HKOG6Li5HyB2MZX8GtuTXQx-ZrJw4",
-  "authDomain": "betufcdata.firebaseapp.com",
-  "databaseURL": "https://betufcdata-default-rtdb.firebaseio.com/",
-  "storageBucket": "betufcdata.appspot.com"
-}
